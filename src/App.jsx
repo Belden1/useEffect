3c7e8b456779b98e68f0b0e22d7e492bc106974c
Kristopher Hellsten <carvajalhellstenmail@gmail.com>
2023-12-14 23:58:15 +0100

    Lesson 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 
+function Card() {
+	useEffect(() => {
+		console.log('I was mounted');
+		return () => {
+			console.log('I was unmounted');
+		};
+	}, []);
+	return (
+		<>
+			<div>Card</div>
+		</>
+	);
+}
+
 function App() {
 	const [count, setCount] = useState(0);
 
@@ -13,6 +27,7 @@ function App() {
 			<div className="App">
 				<button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
 			</div>
+			{count < 5 && <Card />}
 		</>
 	);
 }
