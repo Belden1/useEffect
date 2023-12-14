ca3e24dd328fe16843673a86b91e93070d1585b4
Kristopher Hellsten <carvajalhellstenmail@gmail.com>
2023-12-14 23:45:37 +0100

    Lesson 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-	return (
-		<>
-			<div className="App">App</div>
-		</>
-	);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log(`The value of count is ${count}`);
+  }, [count]);
+
+  return (
+    <>
+      <div className="App">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </>
+  );
 }
 
 export default App;
